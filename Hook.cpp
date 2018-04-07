fbaea406c48d30df97092c9912f46d598d15a473
Speedi13 <Speedi13_im_web@web.de>
2018-04-07 18:25:31 +0200

    Update Hook.cpp

--- Hook.cpp
+++ Hook.cpp
@@ -3,6 +3,7 @@
 /// <summary>Detours the export entry from the export table in memory on Windows x64/x86</summary>
 /// <param name="hModuleBase">address of dll</param>
 /// <param name="pExportToHook">name of the export (this is case sensitive)</param>
+/// <param name="HookFnc">address of the function to call instead</param>
 /// <returns>returns the address of the original export table entry</returns>
 void* HookExportTable( HMODULE hModuleBase, char* pExportToHook, void* HookFnc )
 {
